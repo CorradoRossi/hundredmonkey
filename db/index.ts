f5e73f2ff7568a2550bd764dbef152bfc4116054
Rossi Meacham <rossi@frugaltricks.com>
2021-06-03 20:34:52 -0400

    all new

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,7 @@
+import { enhancePrisma } from "blitz"
 import { PrismaClient } from "@prisma/client"
+
+const EnhancedPrisma = enhancePrisma(PrismaClient)
+
 export * from "@prisma/client"
-
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient()
-} else {
-  // Ensure the prisma instance is re-used during hot-reloading
-  // Otherwise, a new client will be created on every reload
-  global["prisma"] = global["prisma"] || new PrismaClient()
-  prisma = global["prisma"]
-}
-
-export default prisma
+export default new EnhancedPrisma()
