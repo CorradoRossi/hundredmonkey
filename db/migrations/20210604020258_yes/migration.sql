ec423e8eb2d829341d1814cc903260bd99dc83a9
Rossi Meacham <rossi@frugaltricks.com>
2021-06-03 22:03:07 -0400

    hmm

--- db/migrations/20210604020258_yes/migration.sql
+++ db/migrations/20210604020258_yes/migration.sql
@@ -37,6 +37,14 @@ CREATE TABLE "Token" (
     FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "name" TEXT NOT NULL
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User.email_unique" ON "User"("email");
 
